8b35c6c26d1ac32c1f78add1ab4a5ee282dfbdf1
TreyAmador <tmamador@cpp.edu>
2018-01-19 19:06:19 -0800

    reorganized service

--- util/config.js
+++ util/config.js
@@ -43,37 +43,3 @@ module.exports = function(express) {
 
 }
 
-
-/*
-
-module.exports = function(express) {
-    
-    var app = express();
-    var path = require('path');
-    
-    var logger = require('morgan');
-    var cookieParser = require('cookie-parser');
-    var bodyParser = require('body-parser');
-    //var favicon = require('serve-favicon');
-
-    var api = require('./api.js');
-    var errors = require('./error.js');
-    
-    //app.set('view engine','ejs');
-    app.engine('html',require('ejs').renderFile);
-    //app.use(logger('dev'));
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: false}));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname,'../frontend')));
-    
-    //app.use(favicon(path.join(__dirname,'../frontend','img','mesh.png')));
-    
-    app.use('/',api);
-    errors(app);
-    
-    return app;
-
-}
-
-*/
