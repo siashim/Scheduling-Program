893920e233f5759e43d7804d33b6bb5269879419
TreyAmador <tmamador@cpp.edu>
2018-03-07 02:09:37 -0800

    create personal time working in console

--- public/views/profileView/profile.ctrl.js
+++ public/views/profileView/profile.ctrl.js
@@ -1,10 +1,11 @@
 mainapp.controller('profileCtrl', function ($scope, $rootScope, profileFactory){
+   
    $scope.form = {};
    $scope.available = createAvailable();
-
+      
    function createAvailable(){
       var ary = [];
-      for (var i=0; i<=23; i++){
+      for (var i = 0; i <= 23; i++){
          ary.push({
             checked: (i>=8 && i<17) ? true : false,
             slotStart: new Date(0, 0, 0, i, 0, 0, 0),
@@ -37,10 +38,82 @@ mainapp.controller('profileCtrl', function ($scope, $rootScope, profileFactory){
       })
    };
 
-   // On set-available button click, send availability to db
-   $scope.setAvailable = function(){
 
-      console.log(JSON.stringify($scope.form))
+   function getUnavailable(startInput,endInput) {
+
+      var unavailable = $scope.available.filter(x => !x.checked).map(function(x) { 
+         return { slotStart: new Date(x.slotStart), slotEnd: new Date(x.slotEnd) } 
+      });
+      var index = 0;
+      while (index < unavailable.length-1) {
+         if (unavailable[index].slotEnd.getHours() == unavailable[index+1].slotStart.getHours()) {
+            unavailable[index].slotEnd = unavailable[index+1].slotEnd;
+            unavailable.splice(index+1,1);
+         } else {
+            ++index;
+         }
+      }
+
+      return unavailable;
+
+   }
+
+   function createUnavailableSlots(startInput,endInput,unavailable) {
+
+      var startDate = new Date(startInput);
+      var initDate = startDate.getDate();
+      var initMonth = startDate.getMonth();
+      var initYear = startDate.getFullYear();
+
+      var endDate = new Date(endInput);
+      var finDate = endDate.getDate();
+      var finMonth = endDate.getMonth();
+      var finYear = endDate.getFullYear();
+
+      var numDays = Math.ceil((endDate - startDate) / (1000*60*60*24))+1;
+      var persMtgs = [];
+
+      for (var days = 0; days < numDays; ++days) {
+         for (var i = 0; i < unavailable.length; ++i) {
+
+            var unavailStart = unavailable[i].slotStart;            
+            var start = new Date(initYear,initMonth,initDate,unavailStart.getHours());
+            start.setDate(start.getDate()+days);
+
+            var unavailEnd = unavailable[i].slotEnd;
+            var endHours = unavailEnd.getHours();
+            var end = new Date(initYear,initMonth,initDate,endHours);
+            end.setDate(end.getDate()+days);
+            if (endHours == 0) { end.setDate(end.getDate()+1); }
+
+            persMtgs.push({ start:start, end:end });
+
+         }
+      }
+
+      return persMtgs;
+
+   }
+
+
+   // On set-available button click, send availability to db
+   $scope.setAvailable = function() {
+
+      // TODO some personal day stuff:
+      //    set personal to REPLY.PERSONAL = 2
+      //    bring to frontend only for reminder
+      //       and meeting calendar
+      //    clear out each time new personal time is set
+
+      var startInput = $('#date.start').val();
+      var endInput = $('#date.end').val();
+      if (startInput == '' || endInput == '') { return; }
+
+      var unavailable = getUnavailable(startInput,endInput);
+      var persMtgs = createUnavailableSlots(startInput,endInput,unavailable);
+
+      
+
    };
 
    // Refresh data in browser with data from db
@@ -51,11 +124,25 @@ mainapp.controller('profileCtrl', function ($scope, $rootScope, profileFactory){
       }), function(err){
          console.log(err);
       }
+
+
+      // Initialize form input datetime widgets first
+      $('#datetimepicker .date').datepicker({
+        'format': 'm/d/yyyy',
+        'autoclose': true,
+      });
+
+      var startDatetimepicker = document.getElementById('datetimepicker');
+      var endDatetimepicker = new Datepair(startDatetimepicker, {
+         'defaultDateDelta': 0
+      });
+
    };
    
    // Initialize the page with data
    $(document).ready(refresh())
 });
+
 
 mainapp.factory('profileFactory', function($http){
    var factory = {};
